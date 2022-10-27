e366ba0ac4ef63490bf3578d91b0094724fc5fdd
ireneangulo <irene.anguloeso@gmail.com>
2022-10-27 19:39:53 +0200

    he hecho parte del codigo

--- ejercicio7.py
+++ ejercicio7.py
@@ -14,6 +14,21 @@ class Polinonio(object):
             self.grado_mayor= grado
     def obtener_valor(self, grado):
         return self.contenido[int(grado)] 
-
+    def restar(self, polinomio1, polinomio2): 
+        menor= polinomio1 if (polinomio1.grado_mayor< polinomio2.grado_mayor) else polinomio2
+        mayor= polinomio2 if (menor==polinomio1) else polinomio1
+        self.contenido= mayor.contenido
+        for i in range(0, menor.grado_mayor + 1):
+            try: 
+                mayor_valor= 0 if mayor.existe_grado(i) is False else mayor.obtener_valor(i)
+                menor_valor= 0 if menor.existe_grado(i) is False else menor.obtener_valor(i)
+                total = mayor_valor - menor_valor
+                if self.existe_grado (i):
+                    self.elimina_grado(i)
+                if total != 0: 
+                    self.agregar_termino(i, total)
+            except: 
+                self.agregar_termino(i, total)
+                pass
         
        
