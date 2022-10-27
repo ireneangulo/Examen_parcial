a9fb358538ab92e4646409630a6eedd8972e4f34
ireneangulo <irene.anguloeso@gmail.com>
2022-10-27 20:40:37 +0200

    he hecho el ejercicio

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,4 +1,18 @@
 lista= [18, 50, 210, 80, 145, 333, 70, 30]
+#Apartado 1
+for i in lista:
+    if i %10==0 & i <200:
+        print(i)
+    else: 
+        print("No cumple las condiciones")
+#Apartado 2
+for i in lista:
+    if i > 200:
+        print("Stop")
+    else: 
+        print(i)
+        
+    
 #Apartado 3
 lista.sort()
 print(lista)
